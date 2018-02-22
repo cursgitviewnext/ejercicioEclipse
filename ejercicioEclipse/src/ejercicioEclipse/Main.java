c94886eb66ab1dbb98ed22cf71ae70b42c96fc30
cursgitviewnext <36736213+cursgitviewnext@users.noreply.github.com>
2018-02-22 18:14:09 +0100

    canvi missatge

--- ejercicioEclipse/src/ejercicioEclipse/Main.java
+++ ejercicioEclipse/src/ejercicioEclipse/Main.java
@@ -3,7 +3,7 @@ package ejercicioEclipse;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!!");
+		System.out.println("Hola mundo!! Que tal?");
 
 	}
 
